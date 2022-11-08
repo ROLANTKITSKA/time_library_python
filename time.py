5f93fb4952bd22dca0c606a8662aacaa3a4856d4
ROLANTKITSKA <115018154+ROLANTKITSKA@users.noreply.github.com>
2022-11-08 18:10:24 +0200

    Create time.py

---
+++ time.py
@@ -0,0 +1,20 @@
+#using the time library to calculate the time of execution if a function
+
+from timeit import default_timer as timer
+
+
+print("This is a simple calculator")
+
+start=timer()
+def calculator(x,y,z):
+  if (z=="a"):
+    print(x+y)
+  elif (z=="s"):
+    print(x-y)
+end=timer()
+
+x=int(input("Type a number: "))
+y=int(input("Type a number: "))
+z=input("Type a for additon or s for substraction: ")
+
+calculator(x,y,z)
